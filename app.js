fbcf8f016c82b680f9ba67ea0b035770ba0c4900
Habil Tabesh <habil.tabesh134@gmail.com>
2022-04-12 19:39:06 -0400

    done

---
+++ app.js
@@ -0,0 +1,44 @@
+
+//Addition
+var a = 10;
+var b = 20;
+function addSum(param1, param2) {
+    console.log(param1, param2);
+    return param1 + param2;
+}
+addSum(a, b);
+
+
+
+
+//Subtraction
+var c = 45;
+var d = 25;
+function difference(param1, param2){
+    console.log(param1, param2);
+    return param1 - param2;
+}
+difference(c, d);
+
+
+
+
+//Multiplication
+var e = 7;
+var f = 6;
+function product(param1, param2){
+    console.log(param1, param2);
+    return param1 * param2;
+}
+product(e, f);
+
+
+
+//Division
+var g = 100;
+var h = 25;
+function quotient(param1, param2){
+    console.log(param1, param2);
+    return param1 / param2;
+}
+quotient(g, h);
